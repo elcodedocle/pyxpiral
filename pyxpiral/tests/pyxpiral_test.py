67e210cf997d8ac8ac6c2d222c65118748a8a741
elcodedocle <gael.abadin@gmail.com>
2019-01-01 16:57:39 +0100

    fix test message for python3 support

--- pyxpiral/tests/pyxpiral_test.py
+++ pyxpiral/tests/pyxpiral_test.py
@@ -34,7 +34,7 @@ class TestPyxpiral(unittest.TestCase):
 		Test setUp.
 		'''
 		self.ppl = pyxpiral.Pyxpiral()
-		self.message = u"Never go full electro (AKA Keep calm and read bits cycling in squared spirals).".decode('utf-8').encode('ascii')
+		self.message = "Never go full electro (AKA Keep calm and read bits cycling in squared spirals)."
 
 	def test_encode(self):
 		image = self.ppl.encode(self.message)
